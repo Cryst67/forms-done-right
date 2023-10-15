6b9a0309643c1427d9673e7bb742a2a814d7f137
Aryan Swaminath <aryan.swaminath@aol.com>
2023-10-14 20:52:02 -0500

    Setting up create form page

---
+++ app/(landing)/_components/Heading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function Heading() {
+  return (
+    <div className='max-w-3xl space-y-4'>
+      <h1 className='text-3xl sm:text-'>Forms Done Right</h1>
+    </div>
+  );
+}
